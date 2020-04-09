6a9e9d31eb4d188836cf05c157c0279456bce050
Stef Kariotidis <stef.kariotidis@gmail.com>
2020-04-09 12:07:31 +0300

    logger middleware

--- xrplnsapi.go
+++ xrplnsapi.go
@@ -11,6 +11,7 @@ import (
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hi, please check https://xrplns.com/docs")
